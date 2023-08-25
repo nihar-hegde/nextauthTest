1ef45b3d7fa49c7c5143c15a3caaafa187a87c31
Nihar Hegde <niharhegde163@gmail.com>
2023-08-25 22:10:08 +0530

    added vine js validation

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,19 @@
+import { connectToDb } from "@/database/mongo.config";
+import { NextRequest, NextResponse } from "next/server";
+import {registerSchema} from '@/validator/authSchema'
+import vine,{errors} from '@vinejs/vine'
+
+connectToDb();
+
+export async function POST(request:NextRequest){
+    try {
+        const body = await request.json();
+        const validator = vine.compile(registerSchema);
+        const output = await validator.validate(body);
+        return NextResponse.json(output,{status:200})
+    } catch (error) {
+        if(error instanceof errors.E_VALIDATION_ERROR){
+               return NextResponse.json(error.messages,{status:400})
+        }
+    }
+}
---
+++ src/validator/authSchema.ts
@@ -0,0 +1,7 @@
+import vine from '@vinejs/vine'
+
+export const registerSchema = vine.object({
+    name:vine.string().trim().minLength(2).maxLength(50),
+    email:vine.string().email(),
+    password:vine.string().minLength(6).maxLength(20).confirmed(),
+})
